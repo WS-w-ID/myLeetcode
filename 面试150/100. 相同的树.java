64853154cadb112fd1c8df6ca5daf6079b24029e
Lincheng Zhang <1181615030@qq.com>
2024-11-05 19:29:28 +0800

    Create 100. 相同的树.java

---
+++ 面试150/100. 相同的树.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null)  // 都为null
+            return true;
+        else if(p==null || q==null) // 只有一个为null
+            return false;
+        else if(p.val != q.val)    // 都不为null
+            return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
