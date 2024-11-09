ae4c12e0a58efb81b1a62d95cf21618da8c60d12
Lincheng Zhang <1181615030@qq.com>
2024-11-09 11:44:49 +0800

    Create 114. 二叉树展开为链表.java

---
+++ 面试150/114. 二叉树展开为链表.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        /**找到前驱结点 */
+        TreeNode curr = root;
+        while(curr != null){
+            if(curr.left != null){
+                TreeNode next = curr.left;
+                TreeNode pre = next;
+                while(pre.right != null){
+                    pre = pre.right;
+                }
+                pre.right = curr.right;
+                curr.left = null;
+                curr.right = next;
+            }
+            curr = curr.right;
+        }
+    }
+}
