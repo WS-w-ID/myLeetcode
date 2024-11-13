68ad8b60b1847db3a1d74e354c4ce92deb64868f
Lincheng Zhang <1181615030@qq.com>
2024-11-13 11:23:41 +0800

    Create 637. 二叉树的层平均值.java

---
+++ 面试150/637. 二叉树的层平均值.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> ans = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            double size = queue.size(), sum = 0;
+            for(int i=0; i<size; ++i){
+                TreeNode node = queue.poll();
+                if(node.left != null)
+                    queue.offer(node.left);
+                if(node.right != null)
+                    queue.offer(node.right);
+                sum += node.val;
+            }
+            ans.add(sum/size);
+        }
+        return ans;
+    }
+}
