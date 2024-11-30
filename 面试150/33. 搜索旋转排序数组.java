5b9ee525081856b5a13f919df2b2a72106dd490d
Lincheng Zhang <1181615030@qq.com>
2024-11-30 19:41:57 +0800

    Create 33. 搜索旋转排序数组.java

---
+++ 面试150/33. 搜索旋转排序数组.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        /**小的一端最大的一个数都要比大的一端最小的一个数小 */
+        int left=0, right=nums.length-1, n=nums.length;
+        if(n <= 1)
+            return (nums[0]==target || n==0) ? 0 : -1;
+        while(left <= right){
+            int mid = left+(right-left)/2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] >= nums[0]){   // mid在大的那边
+                if(target >= nums[0] && nums[mid] > target){ // target在左边有序侧
+                    right = mid-1;
+                }
+                else
+                    left = mid+1;
+            }
+            else{   // mid在小的那边
+                if(target > nums[mid] && target <= nums[n-1]){ // target在右边有序侧
+                    left = mid+1;
+                }
+                else
+                    right = mid-1;
+            }
+        }
+        return -1;
+    }
+}
